03579f81f681c314c3cb2ebf28aa73c1f66ec5e3
Anwesh43 <anweshthecool0@gmail.com>
2020-12-01 01:38:54 +0530

    adding BringFromCircleLine class

--- bringcirclefromlineview/src/main/java/com/example/bringcirclefromlineview/BringFromCircleLineView.kt
+++ bringcirclefromlineview/src/main/java/com/example/bringcirclefromlineview/BringFromCircleLineView.kt
@@ -161,4 +161,26 @@ class BringFromCircleLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BringFromCircleLine(var i : Int) {
+
+        private var curr : BFCLNode = BFCLNode(0)
+        private var dir : Int = 1
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
