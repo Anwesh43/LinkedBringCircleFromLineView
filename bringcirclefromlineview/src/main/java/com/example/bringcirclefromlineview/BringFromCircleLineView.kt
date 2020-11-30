ef6503c172fbd42f042ba64c3a2b3cb6c1c92474
Anwesh43 <anweshthecool0@gmail.com>
2020-12-01 01:14:39 +0530

    adding constants and libraries

--- bringcirclefromlineview/src/main/java/com/example/bringcirclefromlineview/BringFromCircleLineView.kt
+++ bringcirclefromlineview/src/main/java/com/example/bringcirclefromlineview/BringFromCircleLineView.kt
@@ -1,2 +1,27 @@
 package com.example.bringcirclefromlineview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val deg : Float = 360f
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#FFC107",
+    "#3F51B5",
+    "#FF5722",
+    "#009688"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
